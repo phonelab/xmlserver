84929a9065e3567026efe74446e6684ba9115bdf
Taeyeon Ki <taeyeon.ki@gmail.com>
2012-08-16 11:44:16 -0400

    starting xmlserver

---
+++ manifest/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+
+def push_manifest(requset):
+  pass
+
+def download_manifest(request, meid):
+  pass
---
+++ urls.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+if settings.DEBUG:
+  urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'xmlserver.views.home', name='home'),
+    # url(r'^xmlserver/', include('xmlserver.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+    #
+    ## Manifest 
+    # Push Manifest 
+    # Accepts: {}
+    # Response : {}
+    url(r'^device/$', 'manifest.views.push_manifest'),
+    
+    #
+    ## Manifest
+    #
+    # Download Manifest
+    url(r'^manifest/(?P<meid>[A-Z0-9]\w+)/$', 'manifest.views.download_manifest'),
+
+  )
